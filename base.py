2282e7024b758df30f9e3c2f83b1b9d092b013c6
Levi Kilcher <Levi.Kilcher@nrel.gov>
2016-10-20 15:16:43 -0600

    Initial commit.

---
+++ base.py
@@ -0,0 +1,51 @@
+import numpy as np
+import os
+
+pkg_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+def centers2edges(arr):
+    d = np.diff(arr)
+    outarr = np.empty(len(arr) + 1, dtype=arr.dtype)
+    outarr[1:-1] = arr[:-1] + d / 2
+    outarr[0] = arr[0] - d[0] / 2
+    outarr[-1] = arr[-1] + d[-1] / 2
+    return outarr
+
+
+class powermatrix(object):
+
+    def __init__(self, mat, period, Hs, name=None):
+        self.period = period
+        self.mat = mat
+        self.Hs = Hs
+        self.name = name
+
+    def __repr__(self, ):
+        outstr = '<'
+        if self.name is not None:
+            outstr += "'{}' ".format(self.name)
+        outstr += ('Power Matrix\n  ({} periods from {} to {} s)'
+                   .format(len(self.period),
+                           self.period[0],
+                           self.period[-1]))
+        outstr += ('\n  ({} Hs from {} to {} m)'
+                   .format(len(self.Hs), self.Hs[0], self.Hs[-1]))
+        outstr += '>'
+        return outstr
+
+    @property
+    def period_edges(self, ):
+        return centers2edges(self.period)
+
+    @property
+    def Hs_edges(self, ):
+        return centers2edges(self.Hs)
+
+tmpdat = np.genfromtxt(pkg_dir + '/pelamis.csv', delimiter=',')
+pelamis = powermatrix(mat=tmpdat[1:, 1:],
+                      period=tmpdat[0, 1:],
+                      Hs=tmpdat[1:, 0],
+                      name='Pelamis')
+
+del tmpdat
